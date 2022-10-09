1dea41884454e8512259cb59bdba48e2b35369a5
Felix Leitl <felix.leitl@icloud.com>
2022-10-09 19:31:05 +0200

    Native iOS photo picker

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,21 +6,73 @@
 //
 
 import SwiftUI
+import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var animateImage = true
+    @State private var bipimage = Image("clown")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            bipimage
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundname: "punchSound")
+                    animateImage = false
+                    withAnimation (.spring(response: 0.3, dampingFraction: 0.3)){
+                        animateImage = true
+                    }
+                }
+            
+            Spacer()
+            
+            PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
+                Label("Photo Library", systemImage: "photo.on.rectangle.angled")
+
+            }
+            .onChange(of: selectedPhoto) { newValue in
+                Task{
+                    do{
+                        if let data = try await
+                            newValue?.loadTransferable(type: Data.self){
+                            if let uiImage  = UIImage(data: data){
+                                bipimage = Image(uiImage: uiImage)
+                            }
+                        }
+                    } catch {
+                        print("😡 ERROR: loading failed\(error.localizedDescription)")
+                    }
+                }
+            }
+            
+            
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func playSound(soundname: String){
+        guard let soundFile = NSDataAsset(name: soundname) else {
+            print("Could not read file named \(soundname)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch{
+            print("Error: \(error.localizedDescription) creating audioplayer")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
