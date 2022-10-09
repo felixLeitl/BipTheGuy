69c2fdd9df5b7b211c503c3867a837219d492551
Felix Leitl <felix.leitl@icloud.com>
2022-10-09 18:38:35 +0200

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Felix Leitl on 09.10.22.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
